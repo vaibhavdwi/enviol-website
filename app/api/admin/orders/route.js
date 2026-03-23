dab999773ce5029171fe015a98a94599da092509
vaibhavdwi <dwi.vaibhav@gmail.com>
2026-03-23 17:47:40 +0530

    dev:order issue

--- app/api/admin/orders/route.js
+++ app/api/admin/orders/route.js
@@ -3,6 +3,9 @@ import { Pool } from "pg";
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 export async function POST(req) {
