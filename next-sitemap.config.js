acf1d0e6a173fa47b8798a38d94bcab8716ac0cf
vaibhavdwi <dwi.vaibhav@gmail.com>
2026-05-29 15:22:03 +0530

    dev:sitemap exlusions

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,37 @@
+/** @type {import('next-sitemap').IConfig} */
+
 module.exports = {
-  siteUrl: 'https://enviol.com',  // your domain
-  generateRobotsTxt: true,        // optional, creates robots.txt
+  siteUrl: 'https://enviol.com',
+
+  generateRobotsTxt: true,
+
   changefreq: 'weekly',
+
   priority: 0.7,
+
+  sitemapSize: 5000,
+
+  exclude: [
+    '/admin/*',
+    '/admin/**',
+    '/api/*',
+    '/api/**',
+    '/guest/*',
+    '/guest/**',
+  ],
+
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: [
+          '/admin',
+          '/admin/*',
+          '/api/*',
+          '/guest/*',
+        ],
+      },
+    ],
+  },
 };
